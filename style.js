952f364234c5105ac9ccea39fbd930255e7b72f6
wet toast <124240726+superwettoast@users.noreply.github.com>
2023-05-13 21:41:07 -1200

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,21 @@
+//when a element with the hidden class is clicked, play a type writer effect
+var hidden = true;
+var text = "wet toast#7683";
 
+const typeWriter = (element, speed) => {
+    hidden = false;
+    const textArray = text.split('');
+    element.innerHTML = ''; 
+    textArray.forEach((letter, i) => {
+        setTimeout(() => (element.innerHTML += letter), speed * i);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".hidden").addEventListener("click", function(event){
+        if (hidden === true) {
+            typeWriter(document.querySelector(".hidden"), 50);
+            document.querySelector(".hidden").classList.add("normal-cursor")
+        }  
+    });
+});
